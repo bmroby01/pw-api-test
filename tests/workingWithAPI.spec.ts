a6aa59157535d8a518161c74d06cb8b1cbfe76ea
bmroby01 <brianna124@gmail.com>
2025-02-21 16:12:21 -0500

    Deleted an article with api call

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -55,5 +55,38 @@ test('delete article', async({page, request}) => {
   await page.getByText('This is a test title').click()
   await page.getByRole('button', {name: "Delete Article"}).first().click()
   await page.getByText('Global Feed').click()
+
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test title')
 })
 
+test('create article', async({page, request}) => {
+  await page.getByText('New Article').click()
+  await page.getByRole('textbox', {name:'Article Title'}).fill('Playwright is awesome')
+  await page.getByRole('textbox', {name: 'What\'s this article about?'}).fill('About Playwright')
+  await page.getByRole('textbox', {name: 'Write your article (in markdown)'}).fill('I like to use Playwright for automation')
+  await page.getByRole('button', {name: 'Publish Article'}).click()
+  const articleResponse = await page.waitForResponse('https://conduit-api.bondaracademy.com/api/articles/')
+  const articleResponseBody = await articleResponse.json()
+  const slugId = articleResponseBody.article.slug
+
+  await expect(page.locator('app-article-page h1')).toContainText('Playwright is awesome')
+  await page.getByText('Home').click()
+  await page.getByText('Global Feed').click()
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('Playwright is awesome')
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      "user":{"email":"brianna124@gmail.com","password":"hello123"}
+    }
+  })
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+
+  const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  })
+  expect(deleteArticleResponse.status()).toEqual(204)
+})
